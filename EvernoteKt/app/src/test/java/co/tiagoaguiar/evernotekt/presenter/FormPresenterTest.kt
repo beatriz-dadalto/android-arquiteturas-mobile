3bad740dcee270e6868a38ebe5c70eece62a379a
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-03-07 11:38:46 -0300

    VIPER com TESTES Junit e Mockito

---
+++ EvernoteKt/app/src/test/java/co/tiagoaguiar/evernotekt/presenter/FormPresenterTest.kt
@@ -0,0 +1,50 @@
+package co.tiagoaguiar.evernotekt.presenter
+
+import co.tiagoaguiar.evernotekt.IForm
+import co.tiagoaguiar.evernotekt.data.model.Note
+import com.nhaarman.mockito_kotlin.verify
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import ru.terrakok.cicerone.Cicerone
+
+@RunWith(MockitoJUnitRunner::class)
+class FormPresenterTest {
+    private lateinit var presenter: FormPresenter
+
+    @Mock
+    private lateinit var view: IForm.View
+
+    @Mock
+    private lateinit var interactor: IForm.Interactor
+
+    @Before
+    fun setup() {
+        val cicerone = Cicerone.create()
+        presenter = FormPresenter(view, interactor, cicerone.router)
+    }
+
+    @Test
+    fun `add note with title and body`() {
+        presenter.saveNote("TitleA", "DescA")
+
+        verify(interactor).createNote(Note(title = "TitleA", body = "DescA"))
+    }
+
+    @Test
+    fun `get note clicked`() {
+        presenter.onStart(1)
+
+        verify(interactor).getNote(1)
+    }
+
+    @Test
+    fun `do not save note without body`() {
+        presenter.saveNote("TitleA", body = "")
+
+        verify(view).displayError("Título e nota deve ser informado")
+    }
+}
---
+++ EvernoteKt/app/src/test/java/co/tiagoaguiar/evernotekt/presenter/HomePresenterTest.kt
@@ -0,0 +1,49 @@
+package co.tiagoaguiar.evernotekt.presenter
+
+import co.tiagoaguiar.evernotekt.IHome
+import co.tiagoaguiar.evernotekt.data.model.Note
+import com.nhaarman.mockito_kotlin.verify
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import ru.terrakok.cicerone.Cicerone
+
+@RunWith(MockitoJUnitRunner::class)
+class HomePresenterTest {
+    private lateinit var presenter: HomePresenter
+
+    @Mock
+    private lateinit var view: IHome.View
+
+    @Mock
+    private lateinit var interactor: IHome.Interactor
+
+    @Before
+    fun setup() {
+        val cicerone = Cicerone.create()
+        presenter = HomePresenter(view, interactor, cicerone.router)
+    }
+
+    @Test
+    fun `display notes when query success`() {
+        val list = listOf(
+            Note(id = 1, title = "TitleA", body = "DescA"),
+            Note(id = 2, title = "TitleB", body = "DescB"),
+        )
+
+        presenter.onQuerySuccess(list)
+
+        verify(view).displayNotes(list)
+    }
+
+    @Test
+    fun `load notes`() {
+        presenter.onStart()
+
+        verify(interactor).loadNoteList()
+    }
+
+}
